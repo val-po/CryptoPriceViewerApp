69bbb5f79a140c7ae65174741ae4f1b55665ca67
VP <VP>
2022-01-09 23:26:51 +0200

    commit #1

---
+++ CryptoPriceViewerApp/CryptoService.swift
@@ -0,0 +1,43 @@
+//
+//  CryptoService.swift
+//  CryptoPriceViewerApp
+//
+//  Created by valentine on 09.01.2022.
+//
+
+import Foundation
+import Combine
+
+final class CryptoService {
+    
+    var components: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.coinranking.com"
+        components.path = "/v2/coins/"
+        components.queryItems = [URLQueryItem(name: "base", value: "USD"), URLQueryItem(name: "timePeriod", value: "24h")]
+        return components
+    }
+    
+    func fetchCoins() -> AnyPublisher<CryptoDataContainer, Error> {
+        return URLSession.shared.dataTaskPublisher(for: components.url!)
+            .map { $0.data}
+            .decode(type: CryptoDataContainer.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
+
+struct CryptoDataContainer: Decodable {
+    let status: String
+    let data: CryptoData
+}
+
+struct CryptoData: Decodable {
+    let coins: [Coin]
+}
+
+struct Coin: Decodable, Hashable {
+    let name: String
+    let price: String
+}
